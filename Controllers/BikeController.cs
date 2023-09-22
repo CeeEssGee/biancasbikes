a037afbf9cbe4d23c7aec214b8dc96b1b1f203cb
Courtney Gulledge <courtney.gulledge@gmail.com>
2023-09-22 10:34:57 -0400

    initial commit

--- Controllers/BikeController.cs
+++ Controllers/BikeController.cs
@@ -5,9 +5,9 @@ using Microsoft.EntityFrameworkCore;
 
 namespace BiancasBikes.Controllers;
 
-[ApiController]
-[Route("api/[controller]")]
-public class BikeController : ControllerBase
+[ApiController] // all controllers in program should be decorated with this
+[Route("api/[controller]")] // The BikeController class also has a Route attribute. This tells the framework what route segment should be associated with all of the endpoints in the controller. In this case, "api/[controller]" tells the framework to use the first part of the controller name ("Bike") to create the route. So all of the endpoints in this controller will have URLs that start with "/api/bike" (it is case insensitive)
+public class BikeController : ControllerBase // all controllers in program should inherit ControllerBase
 {
     private BiancasBikesDbContext _dbContext;
 
@@ -16,11 +16,14 @@ public class BikeController : ControllerBase
         _dbContext = context;
     }
 
-    [HttpGet]
-    [Authorize]
-    public IActionResult Get()
+    // A controller contains all of the endpoints for a specific resource. In this case, all endpoints related to the Bikes resource. Usually you will have one controller for each resource available in the API.
+
+    [HttpGet] // It is decorated with the HttpGet attribute to mark it as a GET endpoint, but is technically unnecessary as GET is the default.
+    [Authorize] // Authorize will be covered in the next chapter.
+    public IActionResult Get() // Finally, the Get method is an endpoint.
+
     {
-        return Ok(_dbContext.Bikes.ToList());
+        return Ok(_dbContext.Bikes.ToList()); // The Ok method that gets called inside Get will create an HTTP response with a status of 200, as well as the data that's passed in.
     }
 
 }
